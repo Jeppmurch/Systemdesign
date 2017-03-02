d90e00286087e5a49a53fd1f8a8ba882c3807141
augustaugust <augustedstrom@hotmail.com>
2017-03-02 16:33:04 +0100

    Ändringar i köket

--- public/js/kitchen.js
+++ public/js/kitchen.js
@@ -1,6 +1,31 @@
 /* global sharedVueStuff, Vue, socket */
 'use strict';
 
+function show_layout(src, id) {
+    var img;
+    var dest = document.getElementById("layout");
+    
+    if(img = document.getElementById("imgId")) {
+        if(img.value == id)
+            dest.removeChild(img);
+        else {
+            img.src = src;
+            img.value = id;
+        }
+        
+    }
+    else {
+        img = document.createElement("img");
+        img.src = src;
+        img.id = "imgId";
+        img.value = id;
+        img.width = 600;
+        img.height = 600;
+        dest.appendChild(img);
+    }
+    
+}
+
 new Vue({
   el: '#orders',
   mixins: [sharedVueStuff], // include stuff that goes to both diner and kitchen
